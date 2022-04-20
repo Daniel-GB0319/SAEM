08c92be58994537edb8070d3cf8a10d4997c7344
Divier <divoflopz@gmail.com>
2022-04-20 18:47:01 -0500

    corrected

--- js/login.js
+++ js/login.js
@@ -2,19 +2,21 @@
 
 const url = "https://saem-back.herokuapp.com/api/auth/signin";
 
-fetch(url, {
-    method: 'POST',
-    headers: {
-        'Contet-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        id: document.querySelector("#userName"),
-        contrasena: document.querySelector("#password")
+function login(){
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Contet-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: document.querySelector("#userName"),
+            contrasena: document.querySelector("#password")
+        })
     })
-})
-.then((response) => {
-    alert("Bienvenido a SAEM!");
-})
-    .catch((error) => {
-        alert("algo no salió bien");
+    .then((response) => {
+        alert("Bienvenido a SAEM!");
     })
+        .catch((error) => {
+            alert("algo no salió bien");
+        })
+}
