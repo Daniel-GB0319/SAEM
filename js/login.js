c42381b264d8ac38b39d2133625d608466f17b19
Divier <divoflopz@gmail.com>
2022-04-20 18:06:30 -0500

    js login test

--- js/login.js
+++ js/login.js
@@ -1,3 +1,12 @@
 'use strict'
 
-fetch()
+const url = "https://saem-back.herokuapp.com/api/auth/signin";
+
+fetch(url)
+    .then(response => response.json())
+    .then(function (data){
+        alert("Bienvenido a SAEM");
+    })
+    .catch(function (error){
+        alert("No se puede acceder");
+    })
